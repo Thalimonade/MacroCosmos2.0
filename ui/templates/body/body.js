fed90cad2313272e41f0141d9b1a4d999fafb8df
numerikdavlok <david.pavlik@unil.ch>
2021-04-27 08:26:57 +0200

    details

--- ui/templates/body/body.js
+++ ui/templates/body/body.js
@@ -1,8 +1,10 @@
-/*import '../login/login.js';
+/*
+import '../login/login.js';
 import '../signUp/signUp.js';
 import '../accueils/accueilSoiBlue.js';
 import '../accueils/accueilNvxRouge.js';
-import '../creaProfil/creaProfil.js'; */
+import '../creaProfil/creaProfil.js'; 
+*/
 import './body.html';
 import '../../../lib/routing.js';
 

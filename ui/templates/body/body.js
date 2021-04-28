dde65289910293dc2876efdc25db6f6ba2759372
numerikdavlok <david.pavlik@unil.ch>
2021-04-28 11:03:02 +0200

    route_sans_click

--- ui/templates/body/body.js
+++ ui/templates/body/body.js
@@ -1,10 +1,8 @@
-/*
+import './body.html';
 import '../login/login.js';
 import '../signUp/signUp.js';
 import '../accueils/accueilSoiBlue.js';
 import '../accueils/accueilNvxRouge.js';
 import '../creaProfil/creaProfil.js'; 
-*/
-import './body.html';
 import '../../../lib/routing.js';
 

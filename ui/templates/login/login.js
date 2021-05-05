cee883d88defa970dea84d0f4316c079e00cbf56
numerikdavlok <david.pavlik@unil.ch>
2021-05-05 12:18:22 +0200

    map debut

--- ui/templates/login/login.js
+++ ui/templates/login/login.js
@@ -20,3 +20,24 @@ Template.LoginSignUp.events({
 		});
 	},
 })
+
+Meteor.startup(function() {
+	GoogleMaps.load();
+  });
+  
+  Template.map.helpers({
+	mapOptions: function() {
+	  if (GoogleMaps.loaded()) {
+		return {
+		  center: new google.maps.LatLng(-37.8136, 144.9631),
+		  zoom: 8
+		};
+	  }
+	}
+  });
+
+Template.map.onCreated(function() {
+	GoogleMaps.ready('map', function(map) {
+	   console.log("I'm ready!");
+	});
+  });

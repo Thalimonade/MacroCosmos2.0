8d47b2a9f1515946e700da448f974de3d02692a1
numerikdavlok <david.pavlik@unil.ch>
2021-05-20 17:23:24 +0200

    reactiv var accueil bleu

--- ui/templates/accueils/accueilSoiBlue.js
+++ ui/templates/accueils/accueilSoiBlue.js
@@ -6,6 +6,113 @@ import { Template } from 'meteor/templating';
 import { Messages } from '../../../import/api/messages'; 
 import { error } from 'jquery';
 
+// ReactiveVars 
+// RVar Profil perso
+Template.accueilBleu.onCreated(function(){
+  this.showProfil = new ReactiveVar( false );
+});
+
+Template.accueilBleu.helpers({
+  showProfil: function() {
+    return Template.instance().showProfil.get();
+  }
+});
+
+Template.accueilBleu.events({
+  'change select': function( event, template ) {
+    if ( $( event.target ).val() === "profilPerso" ) {
+      template.showProfil.set( true );
+    } else {
+      template.showProfil.set( false );
+    }
+  }
+});
+
+// RVar Collaborations
+Template.accueilBleu.onCreated(function(){
+  this.showNetwork = new ReactiveVar( false );
+});
+
+Template.accueilBleu.helpers({
+  showNetwork: function() {
+    return Template.instance().showNetwork.get();
+  }
+});
+
+Template.accueilBleu.events({
+  'change select': function( event, template ) {
+    if ( $( event.target ).val() === "reseauPerso" ) {
+      template.showNetwork.set( true );
+    } else {
+      template.showNetwork.set( false );
+    }
+  }
+});
+
+// Rvar carte réseau personnel
+Template.accueilBleu.onCreated(function(){
+  this.showMap = new ReactiveVar( false );
+});
+
+Template.accueilBleu.helpers({
+  showMap: function() {
+    return Template.instance().showMap.get();
+  }
+});
+
+Template.accueilBleu.events({
+  'change select': function( event, template ) {
+    if ( $( event.target ).val() === "mapBlue" ) {
+      template.showMap.set( true );
+    } else {
+      template.showMap.set( false );
+    }
+  }
+});
+
+//RVar chat
+Template.accueilBleu.onCreated(function(){
+  this.showChat = new ReactiveVar( false );
+});
+
+Template.accueilBleu.helpers({
+  showChat: function() {
+    return Template.instance().showChat.get();
+  }
+});
+
+Template.accueilBleu.events({
+  'change select': function( event, template ) {
+    if ( $( event.target ).val() === "chatBlue" ) {
+      template.showChat.set( true );
+    } else {
+      template.showChat.set( false );
+    }
+  }
+});
+
+// RVar feed perso
+Template.accueilBleu.onCreated(function(){
+  this.showFeed = new ReactiveVar( false );
+});
+
+Template.accueilBleu.helpers({
+  showFeed: function() {
+    return Template.instance().showFeed.get();
+  }
+});
+
+Template.accueilBleu.events({
+  'change select': function( event, template ) {
+    if ( $( event.target ).val() === "feedBlue" ) {
+      template.showFeed.set( true );
+    } else {
+      template.showFeed.set( false );
+    }
+  }
+});
+
+// chat
 Template.Chat.helpers({
    messages() {
        return Messages.find();
@@ -33,42 +140,82 @@ Template.Chat.events({
     },
   });
 
-if (Meteor.isClient) {
-  Template.accueilBleu.events({
-    'click #profilPerso' (event) {
-    event.preventDefault();
-    BlazeLayout.render("profilPersoContact")
+// Carte
+Meteor.startup(function() {
+  GoogleMaps.load({key: 'AIzaSyBDLhhz9MZHAh3IFhIzU0cPunkBBDEzXXo'});
+  });
+  
+  if (Meteor.isClient) {
+  var MAP_ZOOM = 15;
+  
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+  
+  Template.map2.onCreated(function() {
+    var self = this;
+  
+    GoogleMaps.ready('map2', function(map) {
+      var marker;
+  
+      // Create and move the marker when latLng changes.
+      self.autorun(function() {
+        var latLng = Geolocation.latLng();
+        if (! latLng)
+          return;
+  
+        // If the marker doesn't yet exist, create it.
+        if (! marker) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latLng.lat, latLng.lng),
+            map: map.instance
+          });
+        }
+        // The marker already exists, so we'll just change its position.
+        else {
+          marker.setPosition(latLng);
+        }
+  
+        // Center and zoom the map view onto the current position.
+        map.instance.setCenter(marker.getPosition());
+        map.instance.setZoom(MAP_ZOOM);
+      });
+    });
+  });
+  
+  Template.map2.helpers({
+    geolocationError: function() {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+    mapOptions: function() {
+      var latLng = Geolocation.latLng();
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        };
+      }
     }
   });
+  }
 
-  Template.accueilBleu.events({
-    'click #reseauPerso' (event) {
-    event.preventDefault();
-    BlazeLayout.render("reseauCollab")
-    }
-  });
 
-  Template.accueilBleu.events({
-    'click #carteBlue' (event) {
-    event.preventDefault();
-    BlazeLayout.render("map")
-    }
-  }); 
 
-  Template.accueilBleu.events({
-    'click #chatBlue' (event) {
-    event.preventDefault();
-    BlazeLayout.render("Chat")
-    }
-  });
 
-  Template.accueilBleu.events({
-    'click #filActuBlue' (event) {
-    event.preventDefault();
-    BlazeLayout.render("FilActuPerso")
-    }
-  });
-} 
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 1
 
--- ui/templates/login/login.js
+++ ui/templates/login/login.js
@@ -39,8 +39,63 @@ Meteor.startup(function() {
 GoogleMaps.load({key: 'AIzaSyBDLhhz9MZHAh3IFhIzU0cPunkBBDEzXXo'});
 });
 
-  
 if (Meteor.isClient) {
+var MAP_ZOOM = 15;
+
+Meteor.startup(function() {
+  GoogleMaps.load();
+});
+
+Template.map.onCreated(function() {
+  var self = this;
+
+  GoogleMaps.ready('map', function(map) {
+    var marker;
+
+    // Create and move the marker when latLng changes.
+    self.autorun(function() {
+      var latLng = Geolocation.latLng();
+      if (! latLng)
+        return;
+
+      // If the marker doesn't yet exist, create it.
+      if (! marker) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latLng.lat, latLng.lng),
+          map: map.instance
+        });
+      }
+      // The marker already exists, so we'll just change its position.
+      else {
+        marker.setPosition(latLng);
+      }
+
+      // Center and zoom the map view onto the current position.
+      map.instance.setCenter(marker.getPosition());
+      map.instance.setZoom(MAP_ZOOM);
+    });
+  });
+});
+
+Template.map.helpers({
+  geolocationError: function() {
+    var error = Geolocation.error();
+    return error && error.message;
+  },
+  mapOptions: function() {
+    var latLng = Geolocation.latLng();
+    // Initialize the map once we have the latLng.
+    if (GoogleMaps.loaded() && latLng) {
+      return {
+        center: new google.maps.LatLng(latLng.lat, latLng.lng),
+        zoom: MAP_ZOOM
+      };
+    }
+  }
+});
+}
+  
+/*if (Meteor.isClient) {
   Template.map.onCreated(function() {
     Meteor.subscribe('markers')
     GoogleMaps.ready('map', function(map) {
@@ -92,4 +147,5 @@ if (Meteor.isClient) {
       }
     }
   });
-}
+}*/
+

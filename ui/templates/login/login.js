fed73a22f42b42439913dc3af1439bf9a79c6090
numerikdavlok <david.pavlik@unil.ch>
2021-05-22 14:40:35 +0200

    page d'ouverture et template dynamique

--- ui/templates/login/login.js
+++ ui/templates/login/login.js
@@ -42,110 +42,59 @@ GoogleMaps.load({key: 'AIzaSyBDLhhz9MZHAh3IFhIzU0cPunkBBDEzXXo'});
 if (Meteor.isClient) {
 var MAP_ZOOM = 15;
 
-Meteor.startup(function() {
-  GoogleMaps.load();
-});
-
-Template.map.onCreated(function() {
-  var self = this;
-
-  GoogleMaps.ready('map', function(map) {
-    var marker;
-
-    // Create and move the marker when latLng changes.
-    self.autorun(function() {
-      var latLng = Geolocation.latLng();
-      if (! latLng)
-        return;
-
-      // If the marker doesn't yet exist, create it.
-      if (! marker) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(latLng.lat, latLng.lng),
-          map: map.instance
-        });
-      }
-      // The marker already exists, so we'll just change its position.
-      else {
-        marker.setPosition(latLng);
-      }
-
-      // Center and zoom the map view onto the current position.
-      map.instance.setCenter(marker.getPosition());
-      map.instance.setZoom(MAP_ZOOM);
-    });
-  });
-});
-
-Template.map.helpers({
-  geolocationError: function() {
-    var error = Geolocation.error();
-    return error && error.message;
-  },
-  mapOptions: function() {
-    var latLng = Geolocation.latLng();
-    // Initialize the map once we have the latLng.
-    if (GoogleMaps.loaded() && latLng) {
-      return {
-        center: new google.maps.LatLng(latLng.lat, latLng.lng),
-        zoom: MAP_ZOOM
-      };
-    }
-  }
-});
-}
-  
-/*if (Meteor.isClient) {
-  Template.map.onCreated(function() {
-    Meteor.subscribe('markers')
-    GoogleMaps.ready('map', function(map) {
-      google.maps.event.addListener(map.instance, 'click', function(event) {
-        Meteor.call('markers.insert', event.latLng.lat(), event.latLng.lng())
-      });
-
-      var markers = {};
-
-      Markers.find().observe({
-        added: function (document) {
-          var marker = new google.maps.Marker({
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: new google.maps.LatLng(document.lat, document.lng),
-            map: map.instance,
-            id: document._id
-          });
-
-          google.maps.event.addListener(marker, 'dragend', function(event) {
-            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-          });
-
-          markers[document._id] = marker;
-        },
-        changed: function (newDocument, oldDocument) {
-          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-        },
-        removed: function (oldDocument) {
-          markers[oldDocument._id].setMap(null);
-          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-          delete markers[oldDocument._id];
-        }
-      });
-    });
-  });
-
   Meteor.startup(function() {
     GoogleMaps.load();
   });
 
+  Template.map.onCreated(function() {
+    var self = this;
+
+    GoogleMaps.ready('map', function(map) {
+      var marker;
+
+      // Création du marker en fonction de lat et lng
+      self.autorun(function() {
+        var latLng = Geolocation.latLng();
+        if (! latLng)
+          return;
+
+        // Si pas de marker, création
+        if (! marker) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latLng.lat, latLng.lng),
+            map: map.instance
+          });
+        }
+        // Si marker, changement de position
+        else {
+          marker.setPosition(latLng);
+        }
+
+        // Zoom sur la position
+        map.instance.setCenter(marker.getPosition());
+        map.instance.setZoom(MAP_ZOOM);
+      });
+    });
+  });
+  
+  // Demande d'activation geoloc du navigateur
   Template.map.helpers({
+    geolocationError: function() {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+
     mapOptions: function() {
-      if (GoogleMaps.loaded()) {
+      var latLng = Geolocation.latLng();
+      // lancement de la carte ggl si on a info sur latlng
+      if (GoogleMaps.loaded() && latLng) {
         return {
-          center: new google.maps.LatLng(46.516,  6.63282),
-          zoom: 8
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
         };
       }
     }
   });
-}*/
+}
+
 
